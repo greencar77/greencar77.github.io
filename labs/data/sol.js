5fe5853b4fa0b74c7f7311b514a1c71919c0e24f
greencar77 <x>
2026-06-08 11:56:20 +0300

    sol path tags

--- labs/data/sol.js
+++ labs/data/sol.js
@@ -5,210 +5,330 @@ let global_sol = {
             "data": {
                 "completed": "2020-11-10",
                 "labId": "DLT57N"
-            }
+            },
+            "pathTags": [
+                "java",
+                "javaapi"
+            ]
         },
         {
             "path": "java/javaapi/restassureddemo",
             "data": {
                 "completed": "2025-10-22",
                 "labId": "M55OE6"
-            }
+            },
+            "pathTags": [
+                "java",
+                "javaapi"
+            ]
         },
         {
             "path": "java/jaxb2demo",
             "data": {
                 "completed": "2025-11-17",
                 "labId": "C235GQ"
-            }
+            },
+            "pathTags": [
+                "java"
+            ]
         },
         {
             "path": "java/jaxb2inherit",
             "data": {
                 "completed": "2026-02-26",
                 "labId": "AM0Q4G"
-            }
+            },
+            "pathTags": [
+                "java"
+            ]
         },
         {
             "path": "java/jaxb3demo",
             "data": {
                 "completed": "2025-11-14",
                 "labId": "7DQSC6"
-            }
+            },
+            "pathTags": [
+                "java"
+            ]
         },
         {
             "path": "java/jdk_features/java09/modularity",
             "data": {
                 "completed": "2026-03-26",
                 "labId": "7ZYDYX"
-            }
+            },
+            "pathTags": [
+                "java",
+                "java09",
+                "jdk_features"
+            ]
         },
         {
             "path": "java/jdk_features/java09/reactivedemo",
             "data": {
                 "completed": "2026-02-15",
                 "labId": "6KUYRS"
-            }
+            },
+            "pathTags": [
+                "java",
+                "java09",
+                "jdk_features"
+            ]
         },
         {
             "path": "java/jdk_features/java16",
             "data": {
                 "completed": "2024-12-06",
                 "labId": "8JXY4S"
-            }
+            },
+            "pathTags": [
+                "java",
+                "jdk_features"
+            ]
         },
         {
             "path": "java/jspecifynull",
             "data": {
                 "completed": "2026-03-13",
                 "labId": "AE1KAD"
-            }
+            },
+            "pathTags": [
+                "java"
+            ]
         },
         {
             "path": "java/map",
             "data": {
                 "completed": "2024-12-06",
                 "labId": "1UL79Q"
-            }
+            },
+            "pathTags": [
+                "java"
+            ]
         },
         {
             "path": "java/spring/hibernate/hibernate-eagerlazy",
             "data": {
                 "completed": "2026-06-05",
                 "labId": "DL8HJG"
-            }
+            },
+            "pathTags": [
+                "hibernate",
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/hibernate/springboot-hibernate-em",
             "data": {
                 "completed": "2026-05-27",
                 "labId": "G14DCI"
-            }
+            },
+            "pathTags": [
+                "hibernate",
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/hibernate/springboot-hibernate-jpa",
             "data": {
                 "completed": "2026-05-25",
                 "labId": "VU740Z"
-            }
+            },
+            "pathTags": [
+                "hibernate",
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/httpjson",
             "data": {
                 "completed": "2026-05-19",
                 "labId": "KZ5HX1"
-            }
+            },
+            "pathTags": [
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/mvcdemo",
             "data": {
                 "completed": "2026-04-21",
                 "labId": "LGPH17"
-            }
+            },
+            "pathTags": [
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/springconf",
             "data": {
                 "completed": "2020-10-16",
                 "labId": "K204NK"
-            }
+            },
+            "pathTags": [
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "java/spring/springcontext",
             "data": {
                 "completed": "2026-05-08",
                 "labId": "PL531K"
-            }
+            },
+            "pathTags": [
+                "java",
+                "spring"
+            ]
         },
         {
             "path": "js/winjs",
             "data": {
                 "completed": "2026-01-30",
                 "labId": "QK290P"
-            }
+            },
+            "pathTags": [
+                "js"
+            ]
         },
         {
             "path": "python/aoc/2024/day01",
             "data": {
                 "completed": "2025-11-15",
                 "labId": "HITYTE"
-            }
+            },
+            "pathTags": [
+                "2024",
+                "aoc",
+                "python"
+            ]
         },
         {
             "path": "python/aoc/2024/day02",
             "data": {
                 "completed": "2025-11-15",
                 "labId": "GT6G23"
-            }
+            },
+            "pathTags": [
+                "2024",
+                "aoc",
+                "python"
+            ]
         },
         {
             "path": "python/aoc/2024/day03",
             "data": {
                 "completed": "2025-12-10",
                 "labId": "YC1XB5"
-            }
+            },
+            "pathTags": [
+                "2024",
+                "aoc",
+                "python"
+            ]
         },
         {
             "path": "python/aoc/2025/day01",
             "data": {
                 "completed": "2025-12-19",
                 "labId": "MUN0SW"
-            }
+            },
+            "pathTags": [
+                "2025",
+                "aoc",
+                "python"
+            ]
         },
         {
             "path": "python/aoc/2025/day02",
             "data": {
                 "completed": "2026-02-21",
                 "labId": "KGU4RY"
-            }
+            },
+            "pathTags": [
+                "2025",
+                "aoc",
+                "python"
+            ]
         },
         {
             "path": "python/jira_query",
             "data": {
                 "completed": "2026-03-03",
                 "labId": "HZE4YC"
-            }
+            },
+            "pathTags": [
+                "python"
+            ]
         },
         {
             "path": "python/libs/playwright",
             "data": {
                 "completed": "2026-06-02",
                 "labId": "BG99P0"
-            }
+            },
+            "pathTags": [
+                "libs",
+                "python"
+            ]
         },
         {
             "path": "python/libs/requests_bin",
             "data": {
                 "completed": "2026-05-19",
                 "labId": "Q6HP1W"
-            }
+            },
+            "pathTags": [
+                "libs",
+                "python"
+            ]
         },
         {
             "path": "python/matplotlib",
             "data": {
                 "completed": "2025-11-23",
                 "labId": "WJ39KX"
-            }
+            },
+            "pathTags": [
+                "python"
+            ]
         },
         {
             "path": "python/numpy_create",
             "data": {
                 "completed": "2025-11-04",
                 "labId": "V8GD3Y"
-            }
+            },
+            "pathTags": [
+                "python"
+            ]
         },
         {
             "path": "python/py_install",
             "data": {
                 "completed": "2026-06-02",
                 "labId": "R0667X"
-            }
+            },
+            "pathTags": [
+                "python"
+            ]
         },
         {
             "path": "python/think_python/metathesis",
             "data": {
                 "completed": "2025-10-24",
                 "labId": "X3PU6D"
-            }
+            },
+            "pathTags": [
+                "python",
+                "think_python"
+            ]
         }
     ]
 };
