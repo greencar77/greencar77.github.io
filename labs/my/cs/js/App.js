bfe1aa7e02825631c55a3ebda5f4e73fd39283fc
greencar77 <x>
2026-07-16 09:57:36 +0300

    CS hibernate - show count and draft list

--- labs/my/cs/js/App.js
+++ labs/my/cs/js/App.js
@@ -2,39 +2,43 @@
 
 class App {
     constructor() {
+        this.prepareData();
+
         let kbList = document.getElementsByTagName("kb");
         for (const kb of kbList) {
+            const tag = kb.getAttribute('value');
             const span = document.createElement("span");
-            span.textContent = "Hello!";
-            span.className = "kb";
+            if (this.tagMap.has(tag)) {
+                span.className = "kb";
+                span.textContent = this.tagMap.get(tag).length;
 
-            span.addEventListener('click', (e) => {
-                const tag = kb.getAttribute('value');
-                this.openPopup(tag);
-            });
+                span.addEventListener('click', (e) => {
+                    this.openPopup(tag);
+                });
 
-            const closeBtn = document.getElementById('close-btn');
+                const closeBtn = document.getElementById('close-btn');
 
-            // Close on button click
-            closeBtn.addEventListener('click', this.closePopup);
+                // Close on button click
+                closeBtn.addEventListener('click', this.closePopup);
 
-            const overlay = document.getElementById('overlay');
+                const overlay = document.getElementById('overlay');
 
-            // Close on click outside the popup
-            overlay.addEventListener('click', (e) => {
-                if (e.target === overlay) {
-                    this.closePopup();
-                }
-            });
+                // Close on click outside the popup
+                overlay.addEventListener('click', (e) => {
+                    if (e.target === overlay) {
+                        this.closePopup();
+                    }
+                });
 
-            // Close on Esc key press
-            document.addEventListener('keydown', (e) => {
-                if (e.key === 'Escape') {
-                    this.closePopup();
-                }
-            });
+                // Close on Esc key press
+                document.addEventListener('keydown', (e) => {
+                    if (e.key === 'Escape') {
+                        this.closePopup();
+                    }
+                });
 
-            kb.appendChild(span);
+                kb.appendChild(span);
+            }
         }
     }
 
@@ -49,19 +53,33 @@ class App {
         const popupLinks = document.getElementById('popup-links');
         popupLinks.innerHTML = '';
 
-        // const links = itemData[itemId] || [];
-        // links.forEach(link => {
-        //     const li = document.createElement('li');
-        //     const a = document.createElement('a');
-        //     a.href = link.url;
-        //     a.textContent = link.text;
-        //     a.target = '_blank';
-        //     li.appendChild(a);
-        //     popupLinks.appendChild(li);
-        // });
+        this.tagMap.get(tag).forEach(task => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = task.path;
+            a.textContent = task.data.labId;
+            a.target = '_blank';
+            li.appendChild(a);
+            popupLinks.appendChild(li);
+        });
 
         const overlay = document.getElementById('overlay');
         overlay.style.display = 'flex';
+    }
+
+    prepareData() {
+        this.tagMap = new Map();
+
+        for (const sol of global_sol.values) {
+            for (const tag of sol.tags) {
+                if (tag.startsWith("entitymanager_")) {
+                    if (!this.tagMap.has(tag)) {
+                        this.tagMap.set(tag, []);
+                    }
+                    this.tagMap.get(tag).push(sol);
+                }
+            }
+        }
     }
 }
 
