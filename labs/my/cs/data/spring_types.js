d258962e34553be9c9dd0714d2ae16d2f0ad37ac
greencar77 <x>
2026-07-21 10:50:25 +0300

    CS new types

---
+++ labs/my/cs/data/spring_types.js
@@ -0,0 +1,16 @@
+let global_spring_types = {
+    "values": [
+        {
+            "canonical": "org.springframework.context.annotation.Bean",
+            "type": "@interface",
+        },
+        {
+            "canonical": "org.springframework.context.annotation.Lazy",
+            "type": "@interface",
+        },
+        {
+            "canonical": "org.springframework.context.annotation.Primary",
+            "type": "@interface",
+        },
+    ]
+};
