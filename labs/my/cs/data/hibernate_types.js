1a2166f901178e2eb4ce254676913c692fc43fc4
greencar77 <x>
2026-07-22 12:25:31 +0300

    cs hib

--- labs/my/cs/data/hibernate_types.js
+++ labs/my/cs/data/hibernate_types.js
@@ -23,6 +23,18 @@ let global_hibernate_types = {
         {
             "canonical": "jakarta.persistence.PersistenceContext",
             "type": "@interface"
+        },
+        {
+            "canonical": "javax.persistence.LockModeType",
+            "type": "enum"
+        },
+        {
+            "canonical": "jakarta.persistence.LockModeType",
+            "type": "enum"
+        },
+        {
+            "canonical": "org.hibernate.annotations.DynamicUpdate",
+            "type": "@interface"
         }
     ]
 };
