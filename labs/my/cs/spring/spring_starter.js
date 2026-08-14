90bebb87f8d0f2adca9483d53c4ec54bc4f6817d
greencar77 <x>
2026-08-14 09:06:38 +0300

    cs springstarter started

---
+++ labs/my/cs/spring/spring_starter.js
@@ -0,0 +1,12 @@
+let global_spring_starter = {
+    "values": [
+        {
+            "groupId": "org.springframework.boot",
+            "artifactId": "spring-boot-starter-web"
+        },
+        {
+            "groupId": "org.springframework.boot",
+            "artifactId": "spring-boot-starter-data-jpa"
+        },
+    ]
+};
